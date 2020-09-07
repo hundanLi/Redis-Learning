f70c269b93c48f32a788b67d399cf05906142d23
hundanLi <zulongli@qq.com>
2020-09-07 13:44:40 +0800

    常用命令:集合，有序集合

--- src/main/java/command/SetTest.java
+++ src/main/java/command/SetTest.java
@@ -3,7 +3,11 @@ package command;
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.api.sync.RedisCommands;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
 
 /**
  * @author hundanli
@@ -14,6 +18,7 @@ public class SetTest {
     RedisClient redisClient;
     RedisCommands<String, String> redisCommands;
     final String key = "set";
+    final String key1 = "set1", key2 = "set2";
 
     @BeforeEach
     public void setup() {
@@ -26,6 +31,51 @@ public class SetTest {
         // 获取同步API，用于执行redis命令
         redisCommands = redisConnection.sync();
         redisCommands.flushall();
+
+    }
+
+    @Test
+    public void saddspopsrem() {
+        redisCommands.sadd(key, "m1", "m2", "m3");
+        Assertions.assertEquals(3, redisCommands.scard(key));
+
+
+        Long srem = redisCommands.srem(key, "m2");
+        Assertions.assertEquals(2, redisCommands.scard(key));
+
+        String spop = redisCommands.spop(key);
+        Assertions.assertFalse(redisCommands.sismember(key, spop));
+
+    }
+
+    @Test
+    public void smembers() {
+        redisCommands.sadd(key, "m1", "m2", "m3");
+        String member = redisCommands.srandmember(key);
+        Assertions.assertTrue(redisCommands.sismember(key, member));
+
+        Set<String> smembers = redisCommands.smembers(key);
+        Assertions.assertEquals(3, smembers.size());
+        smembers.forEach(System.out::println);
+    }
+
+
+    @Test
+    public void setinteract() {
+        redisCommands.sadd(key1, "m1", "m2", "m3");
+        redisCommands.sadd(key2, "m2", "m3", "m4");
+
+        Set<String> sinter = redisCommands.sinter(key1, key2);
+        Assertions.assertEquals(2, sinter.size());
+        System.out.println(sinter);
+
+        Set<String> sunion = redisCommands.sunion(key1, key2);
+        Assertions.assertEquals(4, sunion.size());
+        System.out.println(sunion);
+
+        Set<String> sdiff = redisCommands.sdiff(key1, key2);
+        Assertions.assertEquals(1, sdiff.size());
+        System.out.println(sdiff);
 
     }
 }
---
+++ src/main/java/command/SortedSetTest.java
@@ -0,0 +1,118 @@
+package command;
+
+import io.lettuce.core.Range;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.ScoredValue;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.sound.midi.Soundbank;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author hundanli
+ * @version 1.0.0
+ * @date 2020/9/7 10:56
+ */
+public class SortedSetTest {
+    RedisClient redisClient;
+    RedisCommands<String, String> redisCommands;
+    final String key = "sortedset";
+
+    @BeforeEach
+    public void setup() {
+        String url = "redis://localhost:6379/0";
+        // 创建RedisClient实例
+        redisClient = RedisClient.create(url);
+        // 根据提供的url获取连接
+        StatefulRedisConnection<String, String> redisConnection =
+                redisClient.connect();
+        // 获取同步API，用于执行redis命令
+        redisCommands = redisConnection.sync();
+        redisCommands.flushall();
+
+    }
+
+    @Test
+    public void zaddzrem() {
+        redisCommands.zadd(key, 1.0, "v1");
+        redisCommands.zadd(key, 2.0, "v2");
+        redisCommands.zadd(key, 0.5, "v3");
+        redisCommands.zaddincr(key, 1.5, "v1");
+        Assertions.assertEquals(3, redisCommands.zcard(key));
+
+        redisCommands.zrem(key, "v2");
+        Assertions.assertEquals(2, redisCommands.zcard(key));
+
+    }
+
+    @Test
+    public void zcount() {
+        redisCommands.zadd(key, 1.0, "v1");
+        redisCommands.zadd(key, 2.0, "v2");
+        redisCommands.zadd(key, 0.5, "v3");
+        Assertions.assertEquals(3, redisCommands.zcard(key));
+        Assertions.assertEquals(2, redisCommands.zcount(key, Range.create(1.0, 2.0)));
+
+    }
+
+
+    @Test
+    public void zpop() {
+        redisCommands.zadd(key, 1.0, "v1");
+        redisCommands.zadd(key, 2.0, "v2");
+        redisCommands.zadd(key, 0.5, "v3");
+        Assertions.assertEquals("v3", redisCommands.zpopmin(key).getValue());
+        Assertions.assertEquals("v2", redisCommands.zpopmax(key).getValue());
+    }
+
+
+    @Test
+    public void zincrby() {
+        redisCommands.zadd(key, 1.0, "v1");
+        redisCommands.zincrby(key, 2.0, "v1");
+        ScoredValue<String> scoredValue = redisCommands.zpopmin(key);
+        Assertions.assertEquals(3.0, scoredValue.getScore());
+    }
+
+
+    @Test
+    public void zrange() {
+        redisCommands.zadd(key, 1.0, "v1");
+        redisCommands.zadd(key, 2.0, "v2");
+        redisCommands.zadd(key, 0.5, "v3");
+        List<String> zrange = redisCommands.zrange(key, 1, 2);
+        Assertions.assertEquals(Arrays.asList("v1", "v2"), zrange);
+        List<String> zrangebyscore = redisCommands.zrangebyscore(key, Range.create(0, 1));
+        Assertions.assertEquals(Arrays.asList("v3", "v1"), zrangebyscore);
+
+        List<ScoredValue<String>> scoredValues = redisCommands.zrangeWithScores(key, 1, 2);
+        System.out.println(scoredValues);
+
+        List<String> zrevrange = redisCommands.zrevrange(key, 0, -1);
+        System.out.println(zrevrange);
+
+    }
+
+    @Test
+    public void zrank() {
+        redisCommands.zadd(key, 1.0, "v1");
+        redisCommands.zadd(key, 2.0, "v2");
+        redisCommands.zadd(key, 0.5, "v3");
+        long v2 = redisCommands.zrank(key, "v2");
+        Assertions.assertEquals(2, v2);
+    }
+
+
+    @Test
+    public void zscore() {
+        redisCommands.zadd(key, 1.0, "v1");
+        redisCommands.zadd(key, 2.0, "v2");
+        Double zscore = redisCommands.zscore(key, "v2");
+        Assertions.assertEquals(2.0, zscore);
+    }
+}
